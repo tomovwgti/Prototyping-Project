a2289358d4848f8235cdc43a39482bded4c2768d
Tomo Watanabe <tomovwgti@gmail.com>
2012-03-09 15:38:28 +0900

    Added Voice Contorol for LED sample

---
+++ adk/VoiceLedKit/src/com/pigmal/android/ex/accessory/OutputController.java
@@ -0,0 +1,79 @@
+/*
+ * Copyright (C) 2011 PIGMAL LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.pigmal.android.ex.accessory;
+
+import android.app.Activity;
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
+import android.speech.RecognizerIntent;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.pigmal.android.accessory.Accessory;
+import com.tomovwgti.megaadk.R;
+import com.tomovwgti.megaadk.VoiceLedDemo;
+
+/**
+ * Control the user input and output to ADK
+ * 
+ * @author itog
+ */
+public class OutputController {
+    private static final String TAG = OutputController.class.getSimpleName();
+    private Button balseButton;
+
+    private Activity hostActivity;
+    private ADKCommandSender adkSender;
+
+    public OutputController(Activity activity, Accessory acc) {
+        hostActivity = activity;
+        adkSender = new ADKCommandSender(acc);
+
+        balseButton = (Button) findViewById(R.id.balse);
+        balseButton.setOnClickListener(balseListener);
+    }
+
+    OnClickListener balseListener = new OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            try {
+                // インテント作成
+                Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+                intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,
+                        RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+                intent.putExtra(RecognizerIntent.EXTRA_PROMPT, "VoiceRecognitionTest");
+
+                // インテント発行
+                hostActivity.startActivityForResult(intent, VoiceLedDemo.REQUEST_CODE);
+            } catch (ActivityNotFoundException e) {
+                // このインテントに応答できるアクティビティがインストールされていない場合
+                Toast.makeText(v.getContext(), "ActivityNotFoundException", Toast.LENGTH_LONG)
+                        .show();
+            }
+        }
+    };
+
+    private View findViewById(int id) {
+        return hostActivity.findViewById(id);
+    }
+
+    public void setBalse() {
+        adkSender.balseSequence();
+    }
+}
---
+++ adk/VoiceLedKit/src/com/tomovwgti/megaadk/VoiceLedDemo.java
@@ -0,0 +1,134 @@
+/*
+ * Copyright (C) 2011 PIGMAL LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.tomovwgti.megaadk;
+
+import java.util.ArrayList;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.speech.RecognizerIntent;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import com.pigmal.android.accessory.AccessoryBaseActivity;
+import com.pigmal.android.ex.accessory.ADKCommandReceiver;
+import com.pigmal.android.ex.accessory.OutputController;
+import com.pigmal.android.util.Logger;
+
+public class VoiceLedDemo extends AccessoryBaseActivity {
+    private static final String TAG = VoiceLedDemo.class.getSimpleName();
+    private ADKCommandReceiver mReceiver;
+    public static final int REQUEST_CODE = 0;
+
+    private OutputController mOutputController;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        mReceiver = new ADKCommandReceiver();
+        // mOpenAccessory.setListener(mReceiver);
+
+        if (mOpenAccessory.isConnected()) {
+            showControls();
+        } else {
+            hideControls();
+        }
+    }
+
+    @Override
+    public void onDestroy() {
+        mOpenAccessory.removeListener();
+        super.onDestroy();
+    }
+
+    /**
+     * Show controls on the display
+     */
+    private void showControls() {
+        setContentView(R.layout.main);
+
+        mOutputController = new OutputController(this, mOpenAccessory);
+    }
+
+    /**
+     * Hide all controlls
+     */
+    private void hideControls() {
+        setContentView(R.layout.no_device);
+        mOutputController = null;
+    }
+
+    @Override
+    protected void onUsbAtached() {
+        Logger.v("onUsbAtached");
+        showControls();
+    }
+
+    @Override
+    protected void onUsbDetached() {
+        Logger.v("onUsbDetached");
+        hideControls();
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getTitle().equals("Simulate")) {
+            showControls();
+        } else if (item.getTitle().equals("Quit")) {
+            finish();
+        }
+        return true;
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        menu.add("Simulate");
+        menu.add("Quit");
+        return true;
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        String RESULT_STRING = "バルス";
+
+        // 自分が投げたインテントであれば応答する
+        if (requestCode == REQUEST_CODE && resultCode == RESULT_OK) {
+            String resultsString = "";
+
+            // 結果文字列リスト
+            ArrayList<String> results = data
+                    .getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+
+            for (int i = 0; i < results.size(); i++) {
+                // ここでは、文字列が複数あった場合に結合しています
+                if (results.get(i).equals(RESULT_STRING)) {
+                    mOutputController.setBalse();
+                    Toast.makeText(this, results.get(i), Toast.LENGTH_LONG).show();
+                    return;
+                }
+                resultsString += results.get(i);
+            }
+
+            // トーストを使って結果を表示
+            Toast.makeText(this, resultsString, Toast.LENGTH_LONG).show();
+            return;
+        }
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+}
