c291c33bf9983689e97294ac2a1753bef82cf2af
Tomo Watanabe <tomovwgti@gmail.com>
2012-04-24 19:50:12 +0900

    Added comment

--- Arduino/Blink/Blink.ino
+++ Arduino/Blink/Blink.ino
@@ -4,16 +4,16 @@
   13ピンのLEDを点滅させる
  */
 
-#define LED  13
+#define LED  13  // 13pinにLEDを接続
 
-void setup() {
+void setup() {  // 最初に１度だけ呼ばれる
   // 13pinには1kΩの抵抗が入っているので直接つないでもOK
-  pinMode(LED, OUTPUT);
+  pinMode(LED, OUTPUT);  // LEDのピンを出力として設定
 }
 
 void loop() {
-  digitalWrite(LED, HIGH);   // set the LED on
-  delay(1000);              // wait for a second
-  digitalWrite(LED, LOW);    // set the LED off
-  delay(1000);              // wait for a second
+  digitalWrite(LED, HIGH);   // LEDピンにHIGHを出力
+  delay(1000);               // 1秒間のウェイト
+  digitalWrite(LED, LOW);    // LEDピンにLOWを出力
+  delay(1000);               // 1秒間のウェイト
 }
